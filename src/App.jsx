3cea352a4e24c0509fcc560dedbb360879f137ac
Aaron Weidele <>
2024-10-30 15:59:16 -0400

    Scanner?

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import QRCode from "react-qr-code";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import "./App.css";
 
 const ids = [7028, 9478, 3197, 8897, 7646, 8853, 2103, 6222, 9808, 2118, 3444, 5243];
 
 function App() {
-  const [appMode, setAppMode] = useState("barcodes");
+  const [appMode, setAppMode] = useState("scan");
+  const [data, setData] = useState("Not Found");
+
   const changeAppMode = () => {
     const newMode = appMode === "barcodes" ? "scan" : "barcodes";
     setAppMode(newMode);
@@ -20,10 +23,24 @@ function App() {
       {appMode === "barcodes" && (
         <div className="max-w-2xl m-auto grid grid-cols-3 gap-8">
           {ids.map((id) => (
-            <div key={id} className="card">
+            <div key={id} className="card text-center">
               <QRCode className="w-full aspect-square" value={`${id}`} />
+              <span className="block">{id}</span>
             </div>
           ))}
+        </div>
+      )}
+      {appMode === "scan" && (
+        <div className="max-w-2xl m-auto">
+          <BarcodeScannerComponent
+            width={500}
+            height={500}
+            onUpdate={(err, result) => {
+              if (result) setData(result.text);
+              else setData("Not Found");
+            }}
+          />
+          <p>{data}</p>
         </div>
       )}
     </>
